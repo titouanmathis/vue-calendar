05fe6fd9c7ed7837012a2db9962c5eeaf1c613ca
Titouan Mathis <titouan.mathis@gmail.com>
2016-10-30 22:18:22 +0100

    Add calendar nav

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,15 +1,15 @@
 <template>
-	<table class="calendar">
+	<table :id="id" class="calendar">
 		<thead>
 			<tr>
 				<th>
-					<button @click="prevMonth" class="calendar__nav calendar__nav--prev"><span>➞</span></button>
+					<button type="button" @click="prevMonth" class="calendar__nav calendar__nav--prev"><span>➞</span></button>
 				</th>
 				<th colspan="5" class="calendar__month">
 					{{ month }} {{ year }}
 				</th>
 				<th>
-					<button @click="nextMonth" class="calendar__nav calendar__nav--next"><span>➞</span></button>
+					<button type="button" @click="nextMonth" class="calendar__nav calendar__nav--next"><span>➞</span></button>
 				</th>
 			</tr>
 			<tr>
@@ -20,9 +20,21 @@
 		</thead>
 		<tbody>
 			<tr v-for="week in weeks">
-				<td v-for="day in week.days" :class="`calendar__day${!day.isInMonth ? ' calendar__day--not-in-month' : '' }`">
-					<input type="checkbox" :id="day.id" :checked="day.isChecked" @change="toggleDay">
-					<label :for="day.id"><span>{{ day.label }}</span></label>
+				<td v-for="day in week.days" :class="day.classes()">
+					<input type="checkbox"
+						:id="day.id"
+						:checked="day.isChecked()"
+						@change="toggleDay"
+						@keydown.up="goUp"
+						@keydown.right="goRight"
+						@keydown.down="goDown"
+						@keydown.left="goLeft"
+						:data-date="day.dataDate"
+						:data-up="day.dataUp"
+						:data-right="day.dataRight"
+						:data-down="day.dataDown"
+						:data-left="day.dataLeft">
+					<label :for="day.id"><span>{{ day.label }} {{ day.weekDayNumber }}</span></label>
 				</td>
 			</tr>
 		</tbody>
@@ -30,20 +42,33 @@
 </template>
 
 <script>
+	import store from '../store'
 	import moment from 'moment'
 
+	// Set the locale to FR
+	moment.locale('fr')
+
 	export default {
-		name: 'calendar',
+		name: 'form-date',
 		data () {
 			return {
-				selected: [],
 				weeks: [],
 				moment: new moment()
 			}
 		},
+		props: {
+			id: {
+				type: String
+			}
+		},
 		computed: {
+			selected() {
+				return store.state.dates
+			},
 			weekdaysMin() {
-				return moment.weekdaysMin()
+				const weekdays = moment.weekdaysMin()
+				weekdays.push(weekdays.shift())
+				return weekdays
 			},
 			year() {
 				return this.moment.format('YYYY')
@@ -53,6 +78,51 @@
 			}
 		},
 		methods: {
+
+			goUp(e) {
+				console.log('goUp');
+				console.time('goUp')
+				try {
+					this.$el.querySelector(`#${e.target.getAttribute('data-up')}`).focus()
+				} catch(something) {
+					if (typeof something !== 'undefined') console.log(something);
+				}
+				console.timeEnd('goUp')
+			},
+
+			goRight(e) {
+				console.log('goRight');
+				console.time('goRight');
+				try {
+					this.$el.querySelector(`#${e.target.getAttribute('data-right')}`).focus()
+				} catch(something) {
+					if (typeof something !== 'undefined') console.log(something);
+				}
+				console.timeEnd('goRight')
+			},
+
+			goDown(e) {
+				console.log('goDown');
+				console.time('goDown');
+				try {
+					this.$el.querySelector(`#${e.target.getAttribute('data-down')}`).focus()
+				} catch(something) {
+					if (typeof something !== 'undefined') console.log(something);
+				}
+				console.timeEnd('goDown');
+			},
+
+			goLeft(e) {
+				console.log('goLeft');
+				console.time('goLeft');
+				try {
+					this.$el.querySelector(`#${e.target.getAttribute('data-left')}`).focus()
+				} catch(something) {
+					if (typeof something !== 'undefined') console.log(something);
+				}
+				console.timeEnd('goLeft');
+			},
+
 			/**
 			 * Go to previous month
 			 * @return {void}
@@ -75,10 +145,7 @@
 			 * @return {void}
 			 */
 			toggleDay(e) {
-				const day = e.target.id
-				const index = this.selected.indexOf(day)
-				index < 0 && this.selected.push(day)
-				index > -1 && this.selected.splice(index, 1)
+				store.commit('toggleDate', e.target.getAttribute('data-date'))
 			},
 
 			/**
@@ -109,13 +176,25 @@
 					while (_moment.week() === week.moment.week()) {
 						// Save the current day
 						const day = _moment.clone()
-						const id = day.format('YYYY-MM-DD')
+						const date = day.format('YYYY-MM-DD')
 						// Push the current day
 						week.days.push({
-							id,
+							id: `date-${date}`,
+							dataDate: date,
 							label: day.format('DD'),
-							isChecked: this.selected.indexOf(id) > -1,
-							isInMonth: day.isSame(this.moment, 'month')
+							dataUp: 'date-' + day.clone().subtract(1, 'week').format('YYYY-MM-DD'),
+							dataRight: 'date-' + day.clone().add(1, 'days').format('YYYY-MM-DD'),
+							dataDown: 'date-' + day.clone().add(1, 'week').format('YYYY-MM-DD'),
+							dataLeft: 'date-' + day.clone().subtract(1, 'days').format('YYYY-MM-DD'),
+							classes: () => {
+								const classes = ['calendar__day']
+								if (day.day() === 0 || day.day() === 6) classes.push('calendar__day--weekend')
+								if (!day.isSame(this.moment, 'month')) classes.push('calendar__day--not-in-month')
+								return classes.join(' ')
+							},
+							isChecked() {
+								return store.state.dates.indexOf(date) > -1
+							}
 						})
 						// Go to the next day
 						_moment.add(1, 'days')
@@ -125,16 +204,6 @@
 		},
 		watch: {
 			moment: 'setMonth'
-		},
-		beforeCreate() {
-			// Set the locale to FR
-			moment.locale('fr')
-			// Update the week days order
-			moment.updateLocale('fr', {
-				weekdays: 'Lundi_Mardi_Mercredi_Jeudi_Vendredi_Samedi_Dimanche'.split('_'),
-				weekdaysShort: 'Lun._Mar._Mer._Jeu._Ven._Sam._Dim.'.split('_'),
-				weekdaysMin: 'Lu_Ma_Me_Je_Ve_Sa_Di'.split('_'),
-			})
 		},
 		created() {
 			// Set the calendar
@@ -156,13 +225,13 @@
 
 	.calendar {
 		table-layout: fixed;
-		width: 100%;
-		/*height: 100%;*/
 		text-align: center;
+		border: 1px solid #ccc;
+		-webkit-user-select: none;
 
-		th,
-		td {
-			vertical-align: middle;
+		td,
+		th {
+			min-width: 2em;
 		}
 	}
 
@@ -178,6 +247,13 @@
 		padding: 0;
 		border: 0;
 		background: none;
+		color: inherit;
+
+		&:hover {
+			/*color: #fff;*/
+			background: #eee;
+			-webkit-font-smoothing: antialiased;
+		}
 
 		&:focus {
 			@extend %focus;
@@ -227,6 +303,10 @@
 					color: #fff;
 					background: #222;
 					-webkit-font-smoothing: antialiased;
+
+					&:hover {
+						background: #111;
+					}
 				}
 			}
 		}
@@ -234,7 +314,11 @@
 		label {
 			position: relative;
 			display: block;
+			user-select: none;
 
+			&:hover {
+				background: #eee;
+			}
 
 			&:active {
 
@@ -245,7 +329,15 @@
 
 			span {
 				display: inline-block;
+				user-select: none;
 			}
+		}
+	}
+
+	.calendar__day--weekend {
+
+		label {
+			background: #f5f5f5;
 		}
 	}
 
@@ -253,11 +345,7 @@
 
 		label {
 			color: #ccc;
+			background: none;
 		}
-	}
-
-	* {
-					-webkit-font-smoothing: antialiased;
-
 	}
 </style>
